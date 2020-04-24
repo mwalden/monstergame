ce498f1d665b8093f5de92727cc3df3f676809aa
mwalden <tont0r@hotmail.com>
2020-04-24 15:06:41 -0500

    added camera controller

---
+++ Assets/scripts/utility/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 viewportSize;
+    Camera cam;
+    [Range(0,1)]
+    public float viewPortFactor;
+
+    private Vector3 targetPosition;
+    private Vector3 currentVelocity;
+    public float followDuration;
+    public float maximumFollowSpeed;
+
+    public GameObject blob;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = blob.transform.position - new Vector3(0,0,10);
+        viewportSize = (cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) - cam.ScreenToWorldPoint(Vector2.zero)) * viewPortFactor;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, followDuration, maximumFollowSpeed);
+    }
+
+    void OnDrawGizmos()
+    {
+        
+        Color c = Color.red;
+        c.a = 0.3f;
+        Gizmos.color = c;
+        Gizmos.DrawCube(transform.position, viewportSize );
+    }
+}
+
